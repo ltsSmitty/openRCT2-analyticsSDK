e2fc4b18d4040a314831cfc484dc6eb7c0f3587d
Smitty Penman <smitty.penman@gmail.com>
2024-03-27 12:04:56 -0400

    add data save key to export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export { analytics } from "./objects/analytics";
 export { TrackEventProps, TrackEventType } from "./objects/analytics";
+import { config } from "./config";
+
+const dataSaveKey = config.dataKey;
+
+export { dataSaveKey };
